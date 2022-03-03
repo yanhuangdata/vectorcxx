4ddf840e239c4bf99dc6e9fe85008916611218c7
Jingbo Chen <cj@yanhuangdata.com>
2022-03-03 16:38:27 +0800

    add file sink APIs and add ENVs for cargo and reduce build size by custom features

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod topology;
 use tokio_test::block_on;
 use std::panic;
 use std::sync::{Arc, Mutex};
+use crate::ffi::KafkaSinkParams;
 // use futures::executor::block_on;
 
 
@@ -13,9 +14,21 @@ mod ffi {
 
     }
 
+    pub struct KafkaSinkParams {
+        url: String,
+        topic: String,
+        username: String,
+        password: String
+    }
+
+    pub struct FileSinkParams {
+        target_path: String
+    }
+
     extern "Rust" {
         fn start_topology(file_path: String, data_dir: String) -> bool;
-        fn export_to_kafka(task_id: String, file_path: String, data_dir: String, kafka_server: String, kafka_topic: String) -> ExportResult;
+        fn export_to_kafka(task_id: String, file_path: String, data_dir: String, sink_config: KafkaSinkParams) -> ExportResult;
+        fn export_to_file(task_id: String, file_path: String, data_dir: String, sink_config: FileSinkParams) -> ExportResult;
     }
 }
 
@@ -27,7 +40,7 @@ pub fn start_topology(file_path: String, data_dir: String) -> bool {
     return !result.is_err();
 }
 
-pub fn export_to_kafka(task_id: String, file_path: String, data_dir: String, kafka_server: String, kafka_topic: String) -> ffi::ExportResult {
+pub fn export_to_kafka(task_id: String, file_path: String, data_dir: String, sink_config: KafkaSinkParams) -> ffi::ExportResult {
     let global_buffer = Arc::new(Mutex::new(String::new()));
     let old_hook = panic::take_hook();
 
@@ -45,7 +58,7 @@ pub fn export_to_kafka(task_id: String, file_path: String, data_dir: String, kaf
 
     let result = panic::catch_unwind(|| {
         let (succeed, err_msg) = block_on(
-            topology::export_json_result_to_kafka(task_id, file_path, data_dir, kafka_server, kafka_topic)
+            topology::export_json_result_to_kafka(task_id, file_path, data_dir, sink_config.url, sink_config.topic)
         );
         if succeed {
             return ffi::ExportResult {succeed:true, err_msg: "".to_string()};
@@ -61,6 +74,44 @@ pub fn export_to_kafka(task_id: String, file_path: String, data_dir: String, kaf
         },
         Err(err) => {
             ffi::ExportResult {succeed: false, err_msg: format!("panic in export_to_kafka: {:?}", global_buffer.lock().unwrap())}
+        }
+    }
+}
+
+pub fn export_to_file(task_id: String, file_path: String, data_dir: String, sink_config: ffi::FileSinkParams) -> ffi::ExportResult {
+    let global_buffer = Arc::new(Mutex::new(String::new()));
+    let old_hook = panic::take_hook();
+
+    panic::set_hook({
+        let global_buffer = global_buffer.clone();
+        Box::new(move |info| {
+            let mut global_buffer = global_buffer.lock().unwrap();
+
+            if let Some(s) = info.payload().downcast_ref::<&str>() {
+                let panic_msg = format!("{} in file {}, line {}", s, info.location().unwrap().file(), info.location().unwrap().line());
+                global_buffer.push_str(panic_msg.as_str());
+            }
+        })
+    });
+
+    let result = panic::catch_unwind(|| {
+        let (succeed, err_msg) = block_on(
+            topology::export_json_result_to_file(task_id, file_path, data_dir, sink_config.target_path)
+        );
+        if succeed {
+            return ffi::ExportResult {succeed:true, err_msg: "".to_string()};
+        }
+        return ffi::ExportResult {succeed: false, err_msg};
+    });
+
+    panic::set_hook(old_hook);
+
+    match result {
+        Ok(res) => {
+            ffi::ExportResult {succeed: res.succeed, err_msg: res.err_msg}
+        },
+        Err(err) => {
+            ffi::ExportResult {succeed: false, err_msg: format!("panic in export_to_file: {:?}", global_buffer.lock().unwrap())}
         }
     }
 }
--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -6,9 +6,17 @@
 
 TEST_CASE("vectorcxx api test") {
     try {
-        auto result = vectorcxx::export_to_kafka("00000", "/tmp/data", "/tmp/data_dir", "host.docker.internal:9092", "quickstart-events-1");
+        struct vectorcxx::KafkaSinkParams kafka_params {
+            "host.docker.internal:9092", "quickstart-events-1", "", ""
+        };
+        auto result = vectorcxx::export_to_kafka("00000", "/tmp/data", "/tmp/data_dir", kafka_params);
         auto err_msg = result.err_msg.c_str();
         std::cout << "result: " << result.succeed << ", " << err_msg << std::endl;
+
+        struct vectorcxx::FileSinkParams file_params {"/tmp/data/result_file.log"};
+        auto new_result = vectorcxx::export_to_file("00000", "/tmp/data", "/tmp/data_dir", file_params);
+        auto new_err_msg = result.err_msg.c_str();
+        std::cout << "new result: " << new_result.succeed << ", " << new_err_msg << std::endl;
     } catch (std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
     }
