4bd0d86b7adf9fed217ebab3ebe085d37780905f
Jingbo Chen <cj@yanhuangdata.com>
2022-10-20 17:22:52 +0800

    refine topology and reload config APIs implementation

--- src/topology_controller.rs
+++ src/topology_controller.rs
@@ -1,7 +1,7 @@
 use crate::config_event::{ConfigAction, ConfigEvent};
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Once;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use tracing::{debug, error, info, Level};
 use vector::config::ConfigBuilder;
 use vector::config::ComponentKey;
@@ -9,9 +9,11 @@ use vector::topology::RunningTopology;
 use vector::{config, config::format, metrics, test_util::runtime};
 
 pub struct TopologyController {
-    config_message_sender: Option<tokio::sync::mpsc::Sender<ConfigEvent>>,
     vector_thread_join_handle: Option<std::thread::JoinHandle<()>>,
     generation_id: Arc<AtomicU32>,
+    topology: Arc<Mutex<Option<RunningTopology>>>,
+    config_builder: Arc<Mutex<Option<ConfigBuilder>>>,
+    rt: Arc<tokio::runtime::Runtime>,
 }
 
 static START: Once = Once::new();
@@ -51,7 +53,7 @@ fn _print_ids(config: &mut ConfigBuilder) {
     info!("sink ids: {:?}", sink_ids);
 }
 
-async fn _handle_reload(new: ConfigBuilder, old: &mut ConfigBuilder, topology: &mut RunningTopology) -> bool {
+async fn _handle_reload(mut new: ConfigBuilder, old: &mut ConfigBuilder, topology: &mut RunningTopology) -> bool {
     let new_copy = new.clone();
     match topology
         .reload_config_and_respawn(new.build().unwrap())
@@ -79,7 +81,7 @@ async fn reload_vector(
     config_event: ConfigEvent,
     config_builder: &mut ConfigBuilder,
     topology: &mut RunningTopology,
-) {
+) -> bool {
     debug!("sources before {:?}: {:?}", config_event.action, config_builder.sources);
     debug!("transforms before {:?}: {:?}", config_event.action, config_builder.transforms);
     debug!("sinks before {:?}: {:?}", config_event.action,  config_builder.sinks);
@@ -105,6 +107,7 @@ async fn reload_vector(
             debug!("sinks after {:?}: {:?}", config_event.action, config_builder_new.sinks);
             if !_handle_reload(config_builder_new, config_builder, topology).await {
                 // TODO: handle error here
+                return false;
             }
         }
         ConfigAction::DELETE => {
@@ -124,24 +127,28 @@ async fn reload_vector(
             debug!("sinks after {:?}: {:?}", config_event.action, config_builder_new.sinks);
             if !_handle_reload(config_builder_new, config_builder, topology).await {
                 // TODO: handle error here
+                return false;
             }
         }
         ConfigAction::EXIT => {
             // should not go here
         }
     }
+    true
 }
 
 impl TopologyController {
     pub fn new() -> Self {
         Self {
-            config_message_sender: None,
             vector_thread_join_handle: None,
             generation_id: Arc::new(AtomicU32::new(0)),
+            topology: Arc::new(Mutex::new(None)),
+            config_builder: Arc::new(Mutex::new(None)),
+            rt: Arc::new(runtime()),
         }
     }
 
-    // start a new thread to run a topology
+    // run a topology with tokio runtime
     pub fn start(&mut self, topology_config: &str) -> Result<bool, &'static str> {
         START.call_once(|| {
             setup_logging();
@@ -169,55 +176,22 @@ impl TopologyController {
             metrics::init_global().expect("metrics initialization failed");
         });
 
-        let mut config_builder_copy = config_builder.clone();
-
-        let (config_message_sender, mut config_message_receiver) = tokio::sync::mpsc::channel(1);
-        info!("config update message channel created");
-        self.config_message_sender = Some(config_message_sender);
-
-        let shared_generation_id = self.generation_id.clone();
+        *self.config_builder.lock().unwrap() = Some(config_builder.clone());
+        let topology_cp = self.topology.clone();
         let config = config_builder.build().unwrap();
         info!("config constructed via config builder");
-        let join_handle = std::thread::spawn(|| {
-            let rt = runtime();
 
-            rt.block_on(async move {
-                let (mut topology, _crash) =
+        let rt = self.rt.clone();
+
+        let join_handle = std::thread::spawn(move || {
+
+            rt.block_on(async {
+                let (topology, _crash) =
                     vector::test_util::start_topology(config, false).await;
                 info!("vector topology started");
-                let mut sources_finished = topology.sources_finished();
-
-                loop {
-                    tokio::select! {
-                        Some(config_event) = config_message_receiver.recv() => {
-                            info!("receive config event action={:?} config={:?}",
-                                config_event.action.to_string().as_str(),
-                                config_event.config_str);
-                            match config_event.action {
-                                ConfigAction::EXIT => {
-                                    info!("received exit request");
-                                    break;
-                                },
-                                _ => {
-                                    info!("about to reload vector");
-                                    reload_vector(config_event, &mut config_builder_copy, &mut topology).await;
-                                }
-                            }
-                            increment_generation_id(&shared_generation_id);
-                        }
-                        _ = &mut sources_finished => {
-                            info!("sources finished");
-                            break;
-                        },
-                        else => {
-                            info!("should not reach here")
-                        }
-                    }
-                    // FIXME: why sleep 1 second?
-                    std::thread::sleep(std::time::Duration::from_millis(1000));
-                }
-
-                topology.stop().await;
+                *topology_cp.lock().unwrap() = Some(topology);
+                // no need to handle source_finished here
+                let mut sources_finished = topology_cp.lock().unwrap().as_ref().unwrap().sources_finished();
             });
         });
         info!("vector thread spawned");
@@ -226,22 +200,49 @@ impl TopologyController {
     }
 
     pub fn add_config(&mut self, config: String) -> bool {
-        self.send_config_event("add".to_string(), vec![], config)
+        let res = self.rt.block_on(self.handle_config_event("add".to_string(), vec![], config));
+        if res {
+            increment_generation_id(&self.generation_id);
+        }
+        res
     }
 
     pub fn delete_config(&mut self, config_ids: Vec<String>) -> bool {
-        self.send_config_event("delete".to_string(), config_ids, "".to_string())
+        let res = self.rt.block_on(self.handle_config_event("delete".to_string(), config_ids, "".to_string()));
+        if res {
+            increment_generation_id(&self.generation_id);
+        }
+        res
     }
 
     pub fn update_config(&mut self, config: String) -> bool {
-        self.send_config_event("update".to_string(), vec![], config)
+        let res = self.rt.block_on(self.handle_config_event("update".to_string(), vec![], config));
+        if res {
+            increment_generation_id(&self.generation_id);
+        }
+        res
     }
 
     pub fn exit(&mut self) -> bool {
-        self.send_config_event("exit".to_string(), vec![], "".to_string())
+        // no need to handle config event, stop topology directly.
+        let res = self.stop();
+        if res {
+            increment_generation_id(&self.generation_id);
+        }
+        res
     }
 
     pub fn stop(&mut self) -> bool {
+        // avoid double stop
+        if self.topology.lock().unwrap().as_ref().is_none() {
+            return true;
+        }
+        // here we need to enter runtime context explicitly, or there will be tokio timer panic in
+        // the topology stop method, it's weird.
+        let _guard = self.rt.enter();
+        self.rt.block_on(self.topology.lock().unwrap().take().unwrap().stop());
+        *self.topology.lock().unwrap() = None;
+
         if self.vector_thread_join_handle.is_some() {
             self.vector_thread_join_handle
                 .take()
@@ -259,7 +260,7 @@ impl TopologyController {
     }
 
 
-    fn send_config_event(&self, action: String, ids: Vec<String>, config_str: String) -> bool {
+    async fn handle_config_event(&self, action: String, ids: Vec<String>, config_str: String) -> bool {
         let get_action = |action| match action {
             "init" => ConfigAction::INIT,
             "add" => ConfigAction::ADD,
@@ -269,23 +270,19 @@ impl TopologyController {
             _ => ConfigAction::INIT,
         };
         info!(
-            "about to send vector config event action={:?}",
+            "about to handle vector config event action={:?}",
             action.as_str()
         );
         debug!(
-            "sending config event: action={:?} config={:?}",
+            "handling config event: action={:?} config={:?}",
             action.as_str(),
             config_str
         );
-        self.config_message_sender
-            .as_ref()
-            .unwrap()
-            .try_send(ConfigEvent {
-                action: get_action(action.as_str()),
-                config_ids: ids,
-                config_str,
-            })
-            .unwrap();
-        true
+
+        reload_vector(ConfigEvent {
+            action: get_action(action.as_str()),
+            config_ids: ids,
+            config_str,
+        }, self.config_builder.lock().unwrap().as_mut().unwrap(), self.topology.lock().unwrap().as_mut().unwrap()).await
     }
 }
